06cc9eabb0202eda0f57c14f68d5ae2d9f640101
Justin <justin.bellavance@gmail.com>
2025-01-25 13:04:33 -0500

    basic templating and upgrade

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def test_html():
+    return render_template("test.html")
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_pdf():
+    incomes.append(request.get_json())
+    return '', 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
